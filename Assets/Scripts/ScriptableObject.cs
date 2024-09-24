4749c83317ba1779fe1a1a993fa68b4a7e521dcc
Sali <alvarosali88@gmail.com>
2024-09-24 18:00:27 +0200

    Ejercicio1

--- Assets/Scripts/ScriptableObject.cs
+++ Assets/Scripts/ScriptableObject.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
 public class SpawnManagerScriptableObject : ScriptableObject
 {
+
     public int numeroDeCubos;
 }
